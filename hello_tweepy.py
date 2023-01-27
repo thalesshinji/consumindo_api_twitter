f2f88e0d73e97e11c23467311a792a7965b0a534
Thales <thalesshinji.technik@gmail.com>
2023-01-27 20:34:15 -0300

    Co-authored-by: Nathália Santos <nafernandafw@users.noreply.github.com>

--- hello_tweepy.py
+++ hello_tweepy.py
@@ -1,10 +1,12 @@
 from secrets_1 import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
-
-BRAZIL_WOE_ID = 23424768
-
 import tweepy 
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
 api = tweepy.API(auth)
-trends = api.get_place_trends(BRAZIL_WOE_ID)
+
+public_tweets = api.home_timeline()
+
+for tweet in public_tweets:
+    print(tweet.text)
